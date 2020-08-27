5537a89c0ac88e314c0c0b658573c79b3fee7dac
muchfee <70124354+muchfee@users.noreply.github.com>
2020-08-27 13:28:49 +0700

    Add files via upload

---
+++ Timeconverter.py
@@ -0,0 +1,12 @@
+seconds=int(input("Masukkan detik: ")) #buat dulu variable seconds dengan input untuk memasukan angka
+def timeConverter(seconds): #buat function dengan nama timeconverter
+    hour = seconds // 3600 #Rumus menghitung jam
+    seconds %= 3600  #Agar range tetap di 00-59
+    minutes = seconds //60 #Rumus menhitung menit
+    seconds %=60 #rumus menhitung detik agar range tetap di 00-59
+
+    return "%02d:%02d:%02d" % (hour, minutes, seconds) #ini formatting string agar menjadi ("HH:MM:SS")
+if seconds > 359999: #jika dia lebih dari 35999 maka akan menjadi invalid
+    print("Invalid Input")
+else: #jika tidak makan akan lanjut
+    print(timeConverter(seconds))
